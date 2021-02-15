600822460a300a2c5bf0440ef030b8ec09c3e4a4
tu1026 <s31302@gmail.com>
2021-02-15 11:30:20 -0800

    edit email and use from enviroment instead

--- parse.py
+++ parse.py
@@ -3,16 +3,16 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-
-***REMOVED***
-
-
-
-***REMOVED***
 ***REMOVED***
 ***REMOVED***
 
-Entrez.email = 'linshuan@ubc.ca'
+
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
@@ -31,15 +31,8 @@ Entrez.email = 'linshuan@ubc.ca'
     ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-    # mesh = str(record["MH"]).replace("'", "").replace("[", "").replace("]", "")
 ***REMOVED***
 ***REMOVED***
-    # pt = str(record["PT"]).replace("'", "").replace("[", "").replace("]", "")
-    # au = str(record["AU"])
-    # dp = str(record["DP"])
-    # la = str(record["LA"])
-    # pmc = str(record["PMC"])
-    # si = str(record["SI"])
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
